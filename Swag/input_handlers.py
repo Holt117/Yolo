28b3773df92d418d1c0b403d4e6ffea6dbe9626c
Holt117 <31890981+Holt117@users.noreply.github.com>
2019-03-20 21:25:11 +0300

    Implemented an inventory system and a basic item 	modified:   Swag/components/fighter.py 	modified:   Swag/components/inventory.py 	modified:   Swag/components/item.py 	modified:   Swag/engine.py 	modified:   Swag/game_states.py 	modified:   Swag/input_handlers.py 	new file:   Swag/item_functions.py 	modified:   Swag/map_objects/game_map.py 	modified:   Swag/render_functions.py

--- Swag/input_handlers.py
+++ Swag/input_handlers.py
@@ -8,27 +8,15 @@ def handle_keys(key, game_state):
         return handle_player_turn_keys(key)
     elif game_state == GameStates.PLAYER_DEAD:
         return handle_player_dead_keys(key)
-    elif game_state == GameStates.SHOW_INVENTORY:
+    elif game_state in (GameStates.SHOW_INVENTORY, GameStates.DROP_INVENTORY):
         return handle_inventory_keys(key)
 
-
     return {}
 
 
-def handle_inventory_keys(key):
-    index = key.c - ord('a')
 
-    if index >= 0:
-        return {'inventory_index': index}
 
-    if key.vk == libtcod.KEY_ENTER and key.lalt:
-        # Alt + Enter: toggle full screen
-        return {'fullscreen': True}
-    elif key.vk == libtcod.KEY_ESCAPE:
-        # Exit the menu
-        return {'exit': True}
 
-    return {}
 def handle_player_turn_keys(key):
     key_char = chr(key.c)
 
@@ -53,6 +41,9 @@ def handle_player_turn_keys(key):
         return {'pickup': True}
     elif key_char == 'i':
         return {'show_inventory': True}
+    elif key_char == 'd':
+        return {'drop_inventory': True}
+
     if key.vk == libtcod.KEY_ENTER and key.lalt:
         # Alt+Enter; toggle full screen
         return {'fullscreen': True}
@@ -64,5 +55,27 @@ def handle_player_turn_keys(key):
 
 def handle_player_dead_keys(key):
     key_char = chr(key.c)
-    # No key was pressed
+
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        # Alt+Enter toggle full screen
+        return {'fullscreen': True}
+    elif key.vk == libtcod.KEY_ESCAPE:
+        # Exit the menu
+        return {'exit': True}
     return{}
+
+
+def handle_inventory_keys(key):
+    index = key.c - ord('a')
+
+    if index >= 0:
+        return {'inventory_index': index}
+
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        # Alt + Enter: toggle full screen
+        return {'fullscreen': True}
+    elif key.vk == libtcod.KEY_ESCAPE:
+        # Exit the menu
+        return {'exit': True}
+
+    return {}
